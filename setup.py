8916c5d4be81a3cb023bd948836e345fdcc09912
Luca Soldaini <luca@soldaini.net>
2016-07-10 20:32:38 -0400

    first stub of setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='QuickUMLS',
+    version='0.1dev',
+    packages=['quickumls',],
+    license='MIT License',
+    long_description=open('README.md').read(),
+)
