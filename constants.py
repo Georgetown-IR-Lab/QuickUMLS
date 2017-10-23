19044a15f02e1f29c8e18701253810eb167b5f8f
Luca Soldaini <luca@soldaini.net>
2017-10-23 13:26:17 -0400

    pep8 fixes

--- constants.py
+++ constants.py
@@ -47,29 +47,30 @@ UNICODE_DASHES = {
 }
 
 LANGUAGES = {
-    'BAQ',  #Basque
-    'CHI',  #Chinese
-    'CZE',  #Czech
-    'DAN',  #Danish
-    'DUT',  #Dutch
-    'ENG',  #English
-    'EST',  #Estonian
-    'FIN',  #Finnish
-    'FRE',  #French
-    'GER',  #German
-    'GRE',  #Greek
-    'HEB',  #Hebrew
-    'HUN',  #Hungarian
-    'ITA',  #Italian
-    'JPN',  #Japanese
-    'KOR',  #Korean
-    'LAV',  #Latvian
-    'NOR',  #Norwegian
-    'POL',  #Polish
-    'POR',  #Portuguese
-    'RUS',  #Russian
-    'SCR',  #Croatian
-    'SPA',  #Spanish
-    'SWE',  #Swedish
-    'TUR',  #Turkish
+    'BAQ',  # Basque
+    'CHI',  # Chinese
+    'CZE',  # Czech
+    'DAN',  # Danish
+    'DUT',  # Dutch
+    'ENG',  # English
+    'EST',  # Estonian
+    'FIN',  # Finnish
+    'FRE',  # French
+    'GER',  # German
+    'GRE',  # Greek
+    'HEB',  # Hebrew
+    'HUN',  # Hungarian
+    'ITA',  # Italian
+    'JPN',  # Japanese
+    'KOR',  # Korean
+    'LAV',  # Latvian
+    'NOR',  # Norwegian
+    'POL',  # Polish
+    'POR',  # Portuguese
+    'RUS',  # Russian
+    'SCR',  # Croatian
+    'SPA',  # Spanish
+    'SWE',  # Swedish
+    'TUR',  # Turkish
 }
+
